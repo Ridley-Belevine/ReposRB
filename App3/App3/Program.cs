8b0674ea7c1d1f81f557b66ec7c655531ef7e0fa
Fewhekkes Sweetjeste <bvs04055002@gmail.com>
2025-05-23 19:57:44 +0500

    Задание 3 выполнено.

---
+++ App3/App3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите двузначное число");
+        int number = int.Parse(Console.ReadLine());
+        int digits = number / 10;
+        int units = number % (digits * 10);
+        int sum = digits + units;
+        int mult = digits * units;
+
+        Console.WriteLine($"{digits}, {units}, {sum}, {mult}");
+        Console.ReadKey();
+
+    }
+
+}
