9bb9a602a5c5fcd65339612ea4e3729aab0b0f57
Fewhekkes Sweetjeste <bvs04055002@gmail.com>
2025-06-09 04:54:30 +0500

    Задание 6.2 выполнено.

---
+++ App6.1/App6.1/6(2).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string w = "информатика";
+
+        string WordNumOne = string.Concat(w.Substring(w.IndexOf('ф'), 1),
+                                     w.Substring(w.IndexOf('и'), 1),
+                                     w.Substring(w.IndexOf('р'), 1),
+                                     w.Substring(w.IndexOf('м'), 1),
+                                     w.Substring(w.IndexOf('а'), 1));
+
+        Console.WriteLine(WordNumOne);
+
+        string k = "информатика";
+
+        string WordNumTwo = string.Concat(k.Substring(k.IndexOf('к'), 1),
+                                     k.Substring(k.IndexOf('о'), 1),
+                                     k.Substring(k.IndexOf('р'), 1),
+                                     k.Substring(k.IndexOf('м'), 1),
+                                     k.Substring(k.IndexOf('а'), 1));
+
+        Console.WriteLine(WordNumTwo);
+    }
+}
