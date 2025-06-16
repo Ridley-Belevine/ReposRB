6b0d8d1badd265d422c4cb7ab1cea4fe792dc591
Fewhekkes Sweetjeste <bvs04055002@gmail.com>
2025-06-16 11:13:07 +0500

    Задание 10.4 выполнено.

---
+++ App10.3/App10.3/10(3).cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество чисел в последовательности: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <= 0)
+        {
+            Console.WriteLine("Количество чисел должно быть положительным");
+            return;
+        }
+
+        Console.WriteLine("Введите первое число последовательности: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        bool isOdd = firstNumber % 2 != 0;
+        int sum = firstNumber;
+        int i = 1;
+
+        while (i < n)
+        {
+            Console.WriteLine("Введите следующее число: ");
+            int nextNumber = int.Parse(Console.ReadLine());
+
+            if ((isOdd && nextNumber % 2 != 0) || (!isOdd && nextNumber % 2 == 0))
+            {
+                sum += nextNumber;
+                i++;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+
+        Console.WriteLine($"Полученная сумма последовательности чисел: {sum}");
+    }
+
+}
