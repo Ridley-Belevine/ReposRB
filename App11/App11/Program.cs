2d0c5e3cd62b474590659dfbfae32a1e34baefff
Fewhekkes Sweetjeste <bvs04055002@gmail.com>
2025-06-16 12:21:08 +0500

    3

--- App11/App11/Program.cs
+++ App11/App11/Program.cs
@@ -25,12 +25,14 @@ internal class Program
 
         Console.WriteLine("\nВведите число k для увеличения элементов массива: ");
         int k = int.Parse(Console.ReadLine());
-
         IncreaseArrayElements(progression, k);
         Console.WriteLine("Массив после увеличения: ");
         PrintArray(progression);
 
+        int oddCount = CountOddElements(progression);
+        Console.WriteLine("\nКоличество нечетных элементов: " + oddCount);
     }
+
     static void PrintArray(int[] array)
     {
         for (int i = 0; i < array.Length; i++)
@@ -49,4 +51,19 @@ internal class Program
             array[i] += k;
         }
     }
+
+    static int CountOddElements(int[] array)
+    {
+        int count = 0;
+
+        foreach (int num in array)
+        {
+            if (num % 2 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
