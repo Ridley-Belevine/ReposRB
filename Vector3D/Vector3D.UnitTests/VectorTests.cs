a952d82b693a1bc8f8bb1b0a0c434e1b9c6cc3c5
Fewhekkes Sweetjeste <bvs04055002@gmail.com>
2026-02-07 19:27:32 +0500

    Задание 16 выполнено.

---
+++ Vector3D/Vector3D.UnitTests/VectorTests.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Numerics;
+using NUnit.Framework;
+using Vector3D;
+
+namespace Vector3D.UnitTests
+{
+    [TestFixture]
+    public class VectorTests
+    {
+        [Test]
+        public void ConstructorSetsCoordinates()
+        {
+            var vector = new Vector(1.5, 2.7, 3.9);
+
+            Assert.That(GetX(vector), Is.EqualTo(1.5).Within(1e-10));
+            Assert.That(GetY(vector), Is.EqualTo(2.7).Within(1e-10));
+            Assert.That(GetZ(vector), Is.EqualTo(3.9).Within(1e-10));
+        }
+
+        [Test]
+        public void LengthCalculatesCorrectly()
+        {
+            var vector = new Vector(3, 4, 12);
+            double length = vector.Length;
+
+            Assert.That(length, Is.EqualTo(13).Within(1e-10));
+        }
+
+        [Test]
+        public void LengthWithNegativeComponentsShouldBePositive()
+        {
+            var vector = new Vector(-3, -4, -12);
+            double length = vector.Length;
+
+            Assert.That(length, Is.EqualTo(13).Within(1e-10));
+        }
+
+        [Test]
+        public void ToStringReturnsProperFormat()
+        {
+            var vector = new Vector(1.5, 2.0, 3.75);
+            string result = vector.ToString();
+
+            StringAssert.StartsWith("(", result);
+            StringAssert.Contains(",", result);
+            StringAssert.EndsWith(")", result);
+
+            Assert.That(result, Does.Contain("1.5").Or.Contains("1,5"));
+            Assert.That(result, Does.Contain("2").Or.Contains("2.0").Or.Contains("2,0"));
+            Assert.That(result, Does.Contain("3.75").Or.Contains("3,75"));
+        }
+
+        [Test]
+        public void EqualsWithSameValuesReturnTrue()
+        {
+            var a = new Vector(1.1, 2.2, 3.3);
+            var b = new Vector(1.1, 2.2, 3.3);
+
+            Assert.That(a.Equals(b), Is.True);
+        }
+
+        [Test]
+        public void EqualsWithDifferentValuesReturnFalse()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(4, 5, 6);
+
+            Assert.That(a.Equals(b), Is.False);
+        }
+
+        [Test]
+        public void EqualsWithNonVectorObjectShouldThrowArgumentException()
+        {
+            var vector = new Vector(1, 2, 3);
+            var notVector = "59468958605";
+
+            Assert.Throws<ArgumentException>(() => vector.Equals(notVector));
+        }
+
+        [Test]
+        public void GetHashCodeForEqualVectorsReturnsSameValue()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(1, 2, 3);
+
+            int hash1 = a.GetHashCode();
+            int hash2 = b.GetHashCode();
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void OperatorEqualsWorksCorrectly()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(1, 2, 3);
+            var c = new Vector(4, 5, 6);
+
+            Assert.That(a == b, Is.True);
+            Assert.That(a == c, Is.False);
+        }
+
+        [Test]
+        public void OperatorNotEqualsWorksCorrectly()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(1, 2, 3);
+            var c = new Vector(4, 5, 6);
+
+            Assert.That(a != b, Is.False);
+            Assert.That(a != c, Is.True);
+        }
+
+        [Test]
+        public void OperatorAddWorksCorrectly()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(4, 5, 6);
+            var result = a + b;
+
+            Assert.That(result, Is.EqualTo(new Vector(5, 7, 9)));
+        }
+
+        [Test]
+        public void OperatorSubtractWorksCorrectly()
+        {
+            var a = new Vector(5, 7, 9);
+            var b = new Vector(1, 2, 3);
+            var result = a - b;
+
+            Assert.That(result, Is.EqualTo(new Vector(4, 5, 6)));
+        }
+
+        [TestCase(2.0)]
+        [TestCase(0.5)]
+        [TestCase(-3.0)]
+        [TestCase(0.0)]
+        [TestCase(1.5)]
+        public void OperatorMultiplyVectorByScalarWorksCorrectly(double λ)
+        {
+            var v = new Vector(2, 3, 4);
+            var result = v * λ;
+
+            Assert.That(result, Is.EqualTo(new Vector(2 * λ, 3 * λ, 4 * λ)));
+        }
+
+        [Test]
+        public void OperatorMultiplyScalarByVectorWorksCorrectly()
+        {
+            var v = new Vector(1, 2, 3);
+            double λ = 2.5;
+
+            var result1 = λ * v;
+            var result2 = v * λ;
+
+            Assert.That(result1, Is.EqualTo(result2));
+            Assert.That(result1, Is.EqualTo(new Vector(2.5, 5, 7.5)));
+        }
+
+        [Test]
+        public void DotProductCalculatesCorrectly()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(4, 5, 6);
+
+            double result = a * b;
+
+            Assert.That(result, Is.EqualTo(32).Within(1e-10));
+        }
+
+        [Test]
+        public void DotProductWithOrthogonalVectorsIsZero()
+        {
+            var a = new Vector(1, 0, 0);
+            var b = new Vector(0, 1, 0);
+            double result = a * b;
+
+            Assert.That(result, Is.EqualTo(0).Within(1e-10));
+        }
+
+        [Test]
+        public void CrossProductCalculatesCorrectly()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(4, 5, 6);
+            var result = Vector.Cross(a, b);
+
+            Assert.That(result, Is.EqualTo(new Vector(-3, 6, -3)));
+        }
+
+        [Test]
+        public void CrossProductStandardBasisVectors()
+        {
+            var i = new Vector(1, 0, 0);
+            var j = new Vector(0, 1, 0);
+            var k = new Vector(0, 0, 1);
+
+            Assert.That(Vector.Cross(i, j), Is.EqualTo(k));
+            Assert.That(Vector.Cross(j, k), Is.EqualTo(i));
+            Assert.That(Vector.Cross(k, i), Is.EqualTo(j));
+
+            Assert.That(Vector.Cross(j, i), Is.EqualTo(-k));
+        }
+
+        [Test]
+        public void CrossProductIsOrthogonalToInputVectors()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(4, 5, 6);
+
+            var cross = Vector.Cross(a, b);
+
+            Assert.That(a * cross, Is.EqualTo(0).Within(1e-10),
+                "Векторное произведение должно быть ортогонально первому вектору");
+            Assert.That(b * cross, Is.EqualTo(0).Within(1e-10),
+                "Векторное произведение должно быть ортогонально второму вектору");
+        }
+
+        [Test]
+        public void CrossProductWithParallelVectorsIsZeroVector()
+        {
+            var a = new Vector(1, 2, 3);
+            var b = new Vector(2, 4, 6);
+
+            var result = Vector.Cross(a, b);
+
+            Assert.That(result, Is.EqualTo(new Vector(0, 0, 0)));
+        }
+
+        [Test]
+        public void TestWithPrecisionWithinToleranceShouldBeEqual()
+        {
+            var a = new Vector(1.00000000000001, 2.00000000000002, 3.00000000000003);
+            var b = new Vector(1.00000000000002, 2.00000000000001, 3.00000000000004);
+
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a == b, Is.True);
+        }
+        private double GetX(Vector a) => (double)typeof(Vector)
+                .GetField("x",
+                          System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(a);
+
+        private double GetY(Vector a) => (double)typeof(Vector)
+                .GetField("y",
+                          System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(a);
+
+        private double GetZ(Vector a) => (double)typeof(Vector)
+                .GetField("z",
+                          System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(a);
+    }
+}
---
+++ Vector3D/Vector3D/Vector3D.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector3D
+{
+    public struct Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        private const double Precision = 1e-13;
+
+        public readonly double Length => Math.Sqrt(x * x + y * y + z * z);
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        private static string FormatofCoordinate(double value)
+        {
+
+            if (Math.Abs(value - Math.Round(value)) < Precision)
+            {
+                return Math.Round(value).ToString();
+            }
+
+            return value.ToString("0.#####");
+        }
+        public override string ToString()
+        {
+            return $"({FormatofCoordinate(x)}, {FormatofCoordinate(y)}, {FormatofCoordinate(z)})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Vector))
+            {
+                throw new ArgumentException("Объект не является вектором");
+            }
+
+            Vector other = (Vector)obj;
+            return Math.Abs(x - other.x) < Precision && 
+                   Math.Abs(y - other.y) < Precision &&
+                   Math.Abs(z - other.z) < Precision;
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                const int prime = 23;
+                hash = hash * prime + x.GetHashCode();
+                hash = hash * prime + y.GetHashCode();
+                hash = hash * prime + z.GetHashCode();
+                return hash;
+            }
+        }
+        public static bool operator ==(Vector a, Vector b) => a.Equals(b);
+        public static bool operator !=(Vector a, Vector b) => !a.Equals(b);
+
+        public static Vector operator -(Vector a)
+        {
+            return new Vector(-a.x, -a.y, -a.z);
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector operator *(Vector a, double λ)
+        {
+            return new Vector(a.x * λ, a.y * λ, a.z * λ);
+        }
+
+        public static Vector operator *(double λ, Vector a)
+        {
+            return a * λ;
+        }
+
+        public static double operator *(Vector a, Vector b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+
+        public static Vector Cross(Vector a, Vector b)
+        {
+            return new Vector(
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x
+            );
+        }
+    }
+}
